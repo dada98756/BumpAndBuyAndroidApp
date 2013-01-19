c384347448f1a3aa6d64aa027ce9d9c28e715a4c
nishiwei <nishiwei4209@gmail.com>
2013-01-19 17:31:46 -0500

    preference is done

--- src/com/pennapps/bumpandbuy/Preference.java
+++ src/com/pennapps/bumpandbuy/Preference.java
@@ -1,7 +1,12 @@
 package com.pennapps.bumpandbuy;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 
 public class Preference {
 	private static SharedPreferences sharedpref;
@@ -17,5 +22,34 @@ public class Preference {
 		String name=context.getResources().getString(R.string.pref_name);
 		String mode=context.getResources().getString(R.string.pref_mode);
 		sharedpref=context.getSharedPreferences(name, Integer.valueOf(mode));
+	}
+	
+	public int put(String key,String value){
+		int code;
+		if(sharedpref.contains(key)){
+			code=1;
+		}else{
+			code=0;
+		}
+		Editor editor=sharedpref.edit();
+		editor.putString(key, value);
+		if(editor.commit())
+			return code;
+		else
+			return -1;
+	}
+	
+	public boolean put(Map<String,String> map){
+		Editor editor=sharedpref.edit();
+		Iterator<Entry<String, String>> iterator=map.entrySet().iterator();
+		while(iterator.hasNext()){
+			Entry<String, String> entry=iterator.next();
+			editor.putString(entry.getKey(), entry.getKey());
+		}
+		return editor.commit();
+	}
+	
+	public String get(String key){
+		return sharedpref.getString(key, "");
 	}
 }
