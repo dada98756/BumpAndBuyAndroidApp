1cc36f3cee24d57285588f7c835d67891a1acd49
taomo117 <taomo117@gmail.com>
2013-01-19 01:42:23 -0500

    GCM stub added

--- src/com/pennapps/bumpandbuy/MainActivity.java
+++ src/com/pennapps/bumpandbuy/MainActivity.java
@@ -1,21 +1,88 @@
 package com.pennapps.bumpandbuy;
 
-import com.pennapps.bumpandbuy.R;
+import static com.pennapps.bumpandbuy.CommonUtilities.DISPLAY_MESSAGE_ACTION;
+import static com.pennapps.bumpandbuy.CommonUtilities.EXTRA_MESSAGE;
+import static com.pennapps.bumpandbuy.CommonUtilities.SENDER_ID;
+import static com.pennapps.bumpandbuy.CommonUtilities.SERVER_URL;
 
+import com.google.android.gcm.GCMRegistrar;
+import com.pennapps.bumpandbuy.R;
+import com.pennapps.bumpandbuy.ServerUtilities;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    
+    TextView mDisplay;
+    AsyncTask<Void, Void, Void> mRegisterTask;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        doGCMStuff();
+        checkNotNull(SERVER_URL, "SERVER_URL");
+        checkNotNull(SENDER_ID, "SENDER_ID");
+        // Make sure the device has the proper dependencies.
+        GCMRegistrar.checkDevice(this);
+        // Make sure the manifest was properly set - comment out this line
+        // while developing the app, then uncomment it when it's ready.
+        GCMRegistrar.checkManifest(this);
+        mDisplay = (TextView) findViewById(R.id.);
+        registerReceiver(mHandleMessageReceiver,
+                new IntentFilter(DISPLAY_MESSAGE_ACTION));
+        final String regId = GCMRegistrar.getRegistrationId(this);
+        if (regId.equals("")) {
+            // Automatically registers application on startup.
+            GCMRegistrar.register(this, SENDER_ID);
+        } else {
+            // Device is already registered on GCM, check server.
+            if (GCMRegistrar.isRegisteredOnServer(this)) {
+                // Skips registration.
+                mDisplay.append(getString(R.string.already_registered) + "\n");
+            } else {
+                // Try to register again, but not in the UI thread.
+                // It's also necessary to cancel the thread onDestroy(),
+                // hence the use of AsyncTask instead of a raw thread.
+                final Context context = this;
+                mRegisterTask = new AsyncTask<Void, Void, Void>() {
+
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        boolean registered =
+                                ServerUtilities.register(context, regId);
+                        // At this point all attempts to register with the app
+                        // server failed, so we need to unregister the device
+                        // from GCM - the app will try to register again when
+                        // it is restarted. Note that GCM will send an
+                        // unregistered callback upon completion, but
+                        // GCMIntentService.onUnregistered() will ignore it.
+                        if (!registered) {
+                            GCMRegistrar.unregister(context);
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void result) {
+                        mRegisterTask = null;
+                    }
+
+                };
+                mRegisterTask.execute(null, null, null);
+            }
+        }
     }
-    
+
     @Override
     public void onResume(){
     	super.onResume();
@@ -28,8 +95,30 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
-    public void doGCMStuff(){
-    	
+
+    @Override
+    protected void onDestroy() {
+        if (mRegisterTask != null) {
+            mRegisterTask.cancel(true);
+        }
+        unregisterReceiver(mHandleMessageReceiver);
+        GCMRegistrar.onDestroy(this);
+        super.onDestroy();
     }
+
+    private void checkNotNull(Object reference, String name) {
+        if (reference == null) {
+            throw new NullPointerException(
+                    getString(R.string.error_config, name));
+        }
+    }
+
+    private final BroadcastReceiver mHandleMessageReceiver =
+            new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String newMessage = intent.getExtras().getString(EXTRA_MESSAGE);
+            mDisplay.append(newMessage + "\n");
+        }
+    };
 }
