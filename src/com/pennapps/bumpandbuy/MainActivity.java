86427604c93e6fb0c22128e50ed661de15da3e65
taomo117 <taomo117@gmail.com>
2013-01-19 10:31:53 -0500

    main activity modified

--- src/com/pennapps/bumpandbuy/MainActivity.java
+++ src/com/pennapps/bumpandbuy/MainActivity.java
@@ -4,22 +4,18 @@ import static com.pennapps.bumpandbuy.CommonUtilities.DISPLAY_MESSAGE_ACTION;
 import static com.pennapps.bumpandbuy.CommonUtilities.EXTRA_MESSAGE;
 import static com.pennapps.bumpandbuy.CommonUtilities.SENDER_ID;
 import static com.pennapps.bumpandbuy.CommonUtilities.SERVER_URL;
-
-import com.google.android.gcm.GCMRegistrar;
-import com.pennapps.bumpandbuy.R;
-import com.pennapps.bumpandbuy.ServerUtilities;
-
-import android.os.AsyncTask;
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.google.android.gcm.GCMRegistrar;
 
 public class MainActivity extends Activity {
     
@@ -45,41 +41,7 @@ public class MainActivity extends Activity {
             // Automatically registers application on startup.
             GCMRegistrar.register(this, SENDER_ID);
         } else {
-            // Device is already registered on GCM, check server.
-            if (GCMRegistrar.isRegisteredOnServer(this)) {
-                // Skips registration.
-                mDisplay.append(getString(R.string.already_registered) + "\n");
-            } else {
-                // Try to register again, but not in the UI thread.
-                // It's also necessary to cancel the thread onDestroy(),
-                // hence the use of AsyncTask instead of a raw thread.
-                final Context context = this;
-                mRegisterTask = new AsyncTask<Void, Void, Void>() {
-
-                    @Override
-                    protected Void doInBackground(Void... params) {
-                        boolean registered =
-                                ServerUtilities.register(context, regId);
-                        // At this point all attempts to register with the app
-                        // server failed, so we need to unregister the device
-                        // from GCM - the app will try to register again when
-                        // it is restarted. Note that GCM will send an
-                        // unregistered callback upon completion, but
-                        // GCMIntentService.onUnregistered() will ignore it.
-                        if (!registered) {
-                            GCMRegistrar.unregister(context);
-                        }
-                        return null;
-                    }
-
-                    @Override
-                    protected void onPostExecute(Void result) {
-                        mRegisterTask = null;
-                    }
-
-                };
-                mRegisterTask.execute(null, null, null);
-            }
+        	Log.v(CommonUtilities.TAG, "Already registered");
         }
     }
 
