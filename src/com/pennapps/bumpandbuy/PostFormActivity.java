7a7e8b54d62c3a366c13c52b40e85f64425b29b3
taomo117 <taomo117@gmail.com>
2013-01-19 08:37:12 -0500

    FilePickerIO api stub added

--- src/com/pennapps/bumpandbuy/PostFormActivity.java
+++ src/com/pennapps/bumpandbuy/PostFormActivity.java
@@ -1,8 +1,11 @@
 package com.pennapps.bumpandbuy;
 
-import android.os.Bundle;
+import io.filepicker.FilePicker;
+import io.filepicker.FilePickerAPI;
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
@@ -11,6 +14,7 @@ public class PostFormActivity extends Activity {
 
 	public static final int SellItButtonClick_ID = 1;
 	public static final int WantItButtonClick_ID = 1;
+	private static final String MY_API_KEY = "AUgWdxAXMQoSUahJ3y4D5z";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +48,25 @@ public class PostFormActivity extends Activity {
 		Toast.makeText(getApplicationContext(),
 				"Want button clicked!",
 				Toast.LENGTH_LONG).show();
+		getPhotoFromFilePickerIO();
 	}
 	
-
+	public void getPhotoFromFilePickerIO(){
+		FilePickerAPI.setKey(MY_API_KEY);
+		Intent intent = new Intent(this, FilePicker.class);
+		startActivityForResult(intent, FilePickerAPI.REQUEST_CODE_GETFILE);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode,
+	                                int resultCode, Intent data) {
+	    if (requestCode == FilePickerAPI.REQUEST_CODE_GETFILE) {
+	        if (resultCode != RESULT_OK)
+			//Result was cancelled by the user or there was an error
+	                return;
+	        Uri uri = data.getData();
+	        System.out.println("File path is " + uri.toString());
+	        System.out.println("FPUrl: " + data.getExtras().getString("fpurl"));
+	    }
+	}
 }
